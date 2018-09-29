d95ddaa81f31b0ee5a258a23082f19e803d24f44
Kristo Palo <kristo.palo@gmail.com>
2018-09-29 13:57:34 +0300

    angular app implemented

---
+++ sentry-turret-app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
